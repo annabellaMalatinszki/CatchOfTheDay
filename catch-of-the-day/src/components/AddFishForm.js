daf313449e4171735fec409eb544850ce8a18eb2
muvirag <a.malatinszki@gmail.com>
2019-03-14 14:26:44 +0100

    Create fish object from AddFishForm values on submit

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,17 +1,45 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = event => {
+    event.preventDefault();
+    const fish = {
+      name: this.nameRef.current.value,
+      price: parseFloat(this.priceRef.current.value),
+      status: this.statusRef.current.value,
+      desc: this.descRef.current.value,
+      image: this.imageRef.current.value
+    };
+    console.log(fish);
+  };
+
   render() {
     return (
-      <form className="fish-edit">
-        <input type="text" name="name" placeholder="Name" />
-        <input type="text" name="price" placeholder="Price" />
-        <select name="status">
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input type="text" name="name" placeholder="Name" ref={this.nameRef} />
+        <input
+          type="text"
+          name="price"
+          placeholder="Price"
+          ref={this.priceRef}
+        />
+        <select name="status" ref={this.statusRef}>
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea name="desc" placeholder="Desc" />
-        <input type="text" name="image" placeholder="Image" />
+        <textarea name="desc" placeholder="Desc" ref={this.descRef} />
+        <input
+          type="text"
+          name="image"
+          placeholder="Image"
+          ref={this.imageRef}
+        />
         <button type="submit">+ Add Fish</button>
       </form>
     );
